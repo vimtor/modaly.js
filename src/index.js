c0b66ee6ee6ee7998492e3b292422ff7e2a05a13
Víctor Navarro González <vn4varro@gmail.com>
2019-07-28 18:04:30 +0200

    Add basic modal features (show, hide)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-if (2 == 2) {
-    a;
+import './style.css';
+
+export default class Modaly {
+    constructor(selector) {
+        this.wrapper = document.querySelector(selector);
+
+        this.wrapper.classList.add('modaly-wrapper');
+        this.hide();
+
+        const openTriggers = document.querySelectorAll(`[data-modaly-open='${selector}']`);
+        openTriggers.forEach(trigger => trigger.addEventListener('click', () => this.show()));
+
+        const closeTriggers = this.wrapper.querySelectorAll('[data-modaly-close]');
+        closeTriggers.forEach(trigger => trigger.addEventListener('click', () => this.hide()));
+    }
+
+    show() {
+        this.wrapper.classList.remove('modaly-hidden');
+    }
+
+    hide() {
+        this.wrapper.classList.add('modaly-hidden');
+    }
 }
