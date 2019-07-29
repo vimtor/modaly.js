691b3ec8960f153635e19a3546f9aec198c29da2
Víctor Navarro González <vn4varro@gmail.com>
2019-07-29 12:44:18 +0200

    Add pseudo element command [ci skip]

---
+++ tests/support/commands.js
@@ -0,0 +1,15 @@
+function unquote(str) {
+    return str.replace(/(^")|("$)/g, '');
+}
+
+Cypress.Commands.add(
+    'before',
+    {
+        prevSubject: 'element',
+    },
+    (el, property) => {
+        const win = el[0].ownerDocument.defaultView;
+        const before = win.getComputedStyle(el[0], 'before');
+        return unquote(before.getPropertyValue(property));
+    },
+);
